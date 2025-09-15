117e6be70bd5645155adf8a041855de859f3a622
fabianwunder <143719438+fabianwunder@users.noreply.github.com>
2025-09-15 11:38:54 +0200

    Create beaver.hpp
    Created main header file

---
+++ beaver.hpp
@@ -0,0 +1,20 @@
+#ifndef BEAVER_HPP
+#define BEAVER_HPP
+
+#include "beaver/version.hpp"
+#include "beaver/pow.hpp"
+#include "beaver/log.hpp"
+#include "beaver/log1p.hpp"
+#include "beaver/atan.hpp"
+#include "beaver/dilog.hpp"
+#include "beaver/svdilog.hpp"
+#include "beaver/atanint.hpp"
+#include "beaver/trilog.hpp"
+//#include "BEAVER/g.hpp"
+//#include "BEAVER/h.hpp"
+
+// Users who want only one function can instead:
+//   #include "beaver/log.hpp"
+// (and avoid pulling non-used functions).
+
+#endif // BEAVER_HPP
